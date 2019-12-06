659547e65693bd9c5d7c3563708e49d2024df0ab
zhangzheng <zz7503578>
2019-12-06 14:37:23 +0800

    设备列表界面,刷新功能,侧滑功能配置

--- lib/owon_pages/list_pages/list_page.dart
+++ lib/owon_pages/list_pages/list_page.dart
@@ -1,14 +1,9 @@
-import 'dart:convert';
-import 'dart:math';
-
-import 'package:dio/dio.dart';
+import 'dart:async';
 import 'package:flutter/material.dart';
-import 'package:owon_pct513/owon_providers/theme_provider.dart';
 import '../../owon_utils/owon_log.dart';
-import '../../owon_utils/owon_http.dart';
-import 'package:dio/dio.dart';
-import 'package:provider/provider.dart';
 import '../../res/owon_themeColor.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
+import 'package:flutter_easyrefresh/easy_refresh.dart';
 
 class ListPage extends StatefulWidget {
   @override
@@ -16,6 +11,9 @@ class ListPage extends StatefulWidget {
 }
 
 class _ListPageState extends State<ListPage> {
+  final SlidableController slidableController = SlidableController();
+  EasyRefreshController refreshController = EasyRefreshController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -32,51 +30,98 @@ class _ListPageState extends State<ListPage> {
             ))
           ],
         ),
-        body: ListView.builder(
-            itemCount: 20,
-            itemBuilder: (context, index) {
-              return Container(
-                height: 110,
-                padding: EdgeInsets.fromLTRB(5, 2, 5, 2),
-                child: Card(
-                    shape: RoundedRectangleBorder(
-                        side: BorderSide(
-                          color:
-                              OwonColor().getCurrent(context, "borderNormal"),
-                          width: 1.0,
-                        ),
-                        borderRadius: BorderRadius.all(Radius.circular(16.0))),
-                    child: Container(
-                      padding: EdgeInsets.fromLTRB(15, 0, 15, 0),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: <Widget>[
-                          Column(
-                            mainAxisAlignment: MainAxisAlignment.center,
+        body: EasyRefresh(
+          controller: refreshController,
+          header: ClassicalHeader(
+            textColor: OwonColor().getCurrent(context, "textColor"),
+          ),
+          footer: ClassicalFooter(
+              textColor: OwonColor().getCurrent(context, "textColor"),
+              enableInfiniteLoad: false
+              ),
+          onRefresh: () async {
+            OwonLog.e("refresh");
+          },
+          onLoad: () async {
+            OwonLog.e("load");
+          },
+          child: ListView.builder(
+              itemCount: 20,
+              itemBuilder: (context, index) {
+                return Slidable(
+                  key: Key(index.toString()),
+                  controller: slidableController,
+                  actionPane: SlidableDrawerActionPane(),
+                  actionExtentRatio: 0.25,
+                  enabled: true,
+                  dismissal: SlidableDismissal(
+                    dismissThresholds: {SlideActionType.secondary: 0.6},
+                    child: SlidableDrawerDismissal(),
+                    onDismissed: (actionType) {
+                      OwonLog.e("什么时候调用 啊");
+                    },
+                    onWillDismiss: (actionType) {
+                      return _slideDelete(index);
+                    },
+                  ),
+                  child: Container(
+                    height: 110,
+                    padding: EdgeInsets.fromLTRB(5, 2, 5, 2),
+                    child: Card(
+                        shape: RoundedRectangleBorder(
+                            side: BorderSide(
+                              color: OwonColor()
+                                  .getCurrent(context, "borderNormal"),
+                              width: 1.0,
+                            ),
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(16.0))),
+                        child: Container(
+                          padding: EdgeInsets.fromLTRB(15, 0, 15, 0),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
                             children: <Widget>[
-                              Image.asset(
-                                "assets/images/launch_icon.png",
-                                fit: BoxFit.contain,
-                                height: 40,
+                              Column(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: <Widget>[
+                                  Image.asset(
+                                    "assets/images/launch_icon.png",
+                                    fit: BoxFit.contain,
+                                    height: 40,
+                                  ),
+                                  SizedBox(
+                                    height: 5,
+                                  ),
+                                  Text(
+                                    "PCT513",
+                                    style: TextStyle(
+                                        fontSize: 13,
+                                        color: OwonColor()
+                                            .getCurrent(context, "textColor")),
+                                  ),
+                                ],
                               ),
-                              SizedBox(
-                                height: 5,
-                              ),
-                              Text(
-                                "PCT513",
-                                style: TextStyle(
-                                    fontSize: 13,
-                                    color: OwonColor()
-                                        .getCurrent(context, "textColor")),
-                              ),
+                              getRightWidget(true)
                             ],
                           ),
-                          getRightWidget(true)
-                        ],
-                      ),
-                    )),
-              );
-            }));
+                        )),
+                  ),
+                  secondaryActions: <Widget>[
+                    Container(
+                      padding: EdgeInsets.all(5),
+                      child: IconSlideAction(
+                          caption: 'Delete',
+                          color: Colors.red,
+                          icon: Icons.delete,
+                          closeOnTap: true,
+                          onTap: () {
+                            _tapDelete(index);
+                          }),
+                    ),
+                  ],
+                );
+              }),
+        ));
   }
 
   Widget getRightWidget(bool normal) {
@@ -104,5 +149,51 @@ class _ListPageState extends State<ListPage> {
         ],
       );
     }
+  }
+
+  FutureOr<bool> _slideDelete(int index) {
+    OwonLog.e("====$index");
+    return showDialog<bool>(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: Text('Delete'),
+          content: Text('Item will be deleted'),
+          actions: <Widget>[
+            FlatButton(
+              child: Text('Cancel'),
+              onPressed: () => Navigator.of(context).pop(false),
+            ),
+            FlatButton(
+              child: Text('Ok'),
+              onPressed: () => Navigator.of(context).pop(true),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  _tapDelete(int index) {
+    OwonLog.e("点击了删除index=$index");
+    return showDialog<bool>(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: Text('Delete'),
+          content: Text('Item will be deleted'),
+          actions: <Widget>[
+            FlatButton(
+              child: Text('Cancel'),
+              onPressed: () => Navigator.of(context).pop(false),
+            ),
+            FlatButton(
+              child: Text('Ok'),
+              onPressed: () => Navigator.of(context).pop(true),
+            ),
+          ],
+        );
+      },
+    );
   }
 }
