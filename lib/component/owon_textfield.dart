1a642960a8a508e4cdca5f3299cfabac99be8e00
Woodie001 <46661180+Woodie001@users.noreply.github.com>
2019-12-13 19:38:41 +0800

    1:textfield修饰

--- lib/component/owon_textfield.dart
+++ lib/component/owon_textfield.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
-import 'package:owon_pct513/res/owon_constant.dart';
 import '../res/owon_themeColor.dart';
 
 class OwonTextField {
@@ -9,51 +8,41 @@ class OwonTextField {
       {double width = double.infinity}) {
     return Container(
       width: width,
-      child: Theme(
-        data: ThemeData(
-            primaryColor: OwonColor().getCurrent(context, "textfieldColor"),
-            highlightColor: Colors.blue),
-        child: TextField(
-            controller: textEditingController,
-            maxLines: 1,
-            autofocus: false,
-            textAlign: TextAlign.left,
-            style: TextStyle(
-                color: OwonColor().getCurrent(context, "textColor"),
-                fontSize: 20.0),
-            keyboardType: TextInputType.text,
-            decoration: InputDecoration(
-              contentPadding:
-                  const EdgeInsets.symmetric(vertical: 10.0, horizontal: 15.0),
-              filled: false,
-//            fillColor: OwonColor().getCurrent(context, "itemColor"),
-//              hoverColor: Colors.red,
-//              fillColor: Colors.blue,
-              prefixIcon: Padding(
-                padding: const EdgeInsets.only(top: 15, bottom: 15),
-                child: Container(
-                    child: SvgPicture.asset(
-                  imageUrl,
-                  color: OwonColor().getCurrent(context, "orange"),
-                  width: 5,
-                )),
-              ),
-//            border: InputBorder(bo),
-//            enabledBorder: OutInputBorder(
-//              borderSide: BorderSide(
-//                  color: Colors.red),
-//            ),
-//            focusedBorder: OutlineInputBorder(
-//              borderSide: BorderSide(
-//                  color: OwonColor().getCurrent(
-//                      context, "borderNormal")),
-//            ),
-              hintText: hint,
-              hintStyle: TextStyle(
-                  fontSize: 14,
-                  color: OwonColor().getCurrent(context, "textColor")),
-            )),
-      ),
+      child: TextField(
+          controller: textEditingController,
+          maxLines: 1,
+          autofocus: false,
+          textAlign: TextAlign.left,
+          style: TextStyle(
+              color: OwonColor().getCurrent(context, "textColor"),
+              fontSize: 20.0),
+          keyboardType: TextInputType.text,
+          decoration: InputDecoration(
+            contentPadding:
+                const EdgeInsets.symmetric(vertical: 10.0, horizontal: 15.0),
+            filled: false,
+            prefixIcon: Padding(
+              padding: const EdgeInsets.only(top: 15, bottom: 15),
+              child: Container(
+                  child: SvgPicture.asset(
+                imageUrl,
+                color: OwonColor().getCurrent(context, "orange"),
+                width: 5,
+              )),
+            ),
+            enabledBorder: UnderlineInputBorder(
+              borderSide: BorderSide(
+                  color: OwonColor().getCurrent(context, "textfieldColor")),
+            ),
+            focusedBorder: UnderlineInputBorder(
+              borderSide: BorderSide(
+                  color: OwonColor().getCurrent(context, "textfieldColor")),
+            ),
+            hintText: hint,
+            hintStyle: TextStyle(
+                fontSize: 14,
+                color: OwonColor().getCurrent(context, "textColor")),
+          )),
     );
   }
 }
